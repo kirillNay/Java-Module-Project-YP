ca7cf21a1fbd032cb885face23997b636b642e07
RomanValeew <vromani1981@gmail.com>
2024-05-08 13:03:15 +0300

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,34 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int persons = BillCalculator.amountRequest();
+
+
+    }
+}
+
+class BillCalculator {
+
+    public static int amountRequest() {
+        Scanner scanner = new Scanner(System.in);
+        int personsRequest = 0;
+
+        while (personsRequest <= 1) {
+            System.out.println("Уточните, пожалуйста, на сколько персон необходимо разделить счет?\nКоличество: ");
+            personsRequest = scanner.nextInt();
+            if (personsRequest == 1) {
+                System.out.println("Тогда нет смысла делить счет :)");
+            } else if (personsRequest <= 0) {
+                System.out.println("Введено некорректное значение. Попробуйте снова.");
+            }
+        }
+        return personsRequest;
+    }
+
+
+    public static  addProducts() {
+
     }
 }
