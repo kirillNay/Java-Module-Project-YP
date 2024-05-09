c92a8e820709e7d0e1779c0d632a7b4e42353a81
Roman.Valeev <vromani1981@gmail.com>
2024-05-09 21:01:29 +0300

    2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,33 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
         int persons = BillCalculator.amountRequest();
+        HashMap<ArrayList<String>, Float> finalProductMap = BillCalculator.Calculator();
+        Map.Entry<ArrayList<String>, Float> entry = finalProductMap.entrySet().iterator().next();
+        ArrayList<String> products = entry.getKey();
+        float totalPaid = entry.getValue();
 
+        System.out.println("--------------------");
+
+        System.out.println("Добавленные товары:");
+        for (String element : products) {
+            System.out.println(element);
+        }
+        System.out.println("--------------------");
+
+        float payForPerson = totalPaid / persons;
+        System.out.printf("Сумма, которую должен заплатить каждый человек, составляет %.2f рубл%s.\n", payForPerson,
+                BillCalculator.properTextEnd(payForPerson));
+
+        System.out.println("--------------------");
 
     }
 }
 
 class BillCalculator {
+
 
     // Запрос и проверка корректности количества человек
     public static int amountRequest() {
@@ -32,11 +47,39 @@ class BillCalculator {
     }
 
 
+    // Метод getCost для try-catch
+    public static float getCost() {
+        Scanner scanner = new Scanner(System.in);
+        float getCost = -1.0f;
+        while (getCost < 0) {
+            getCost = scanner.nextFloat();
+            if (getCost < 0) {
+                System.out.println("Некорректное значение стоимости.\nВведите стоимость товара: ");
+            }
+        }
+        return getCost;
+    }
+
+
+    //Обработка окончаний для вывода
+    public static String properTextEnd(float inCost) {
+        String textEnd = "";
+        int temp = (int)inCost % 10;
+        if (temp == 1) {
+            textEnd = "ь";
+        } else if (temp > 1 & temp < 5) {
+            textEnd = "я";
+        } else {
+            textEnd = "ей";
+        }
+        return textEnd;
+    }
+
+
     // Калькулятор товаров
     public static HashMap<ArrayList<String>, Float> Calculator() {
 
         Scanner scanner = new Scanner(System.in);
-
         HashMap<ArrayList<String>, Float> productHashMap = new HashMap<>();
 
         String command = "";
@@ -50,26 +93,24 @@ class BillCalculator {
 
             //Запрос и проверка ввода стоимости
             while (true) {
+                System.out.println("Введите стоимость товара: ");
                 try {
-                    System.out.println("Введите стоимость товара: ");
-                    productCost = scanner.nextFloat();
+                    productCost = getCost();
                     break;
                 } catch (InputMismatchException e) {
-                    System.out.println("Некорректное значение стоимости. Стоимость должна быть числом в формате xx.xx");
+                    System.out.println("Некорректное значение стоимости.");
                 }
             }
 
             productList.add(productName);
             totalCost = totalCost + productCost;
-            System.out.println("Товар '" + productName + "'стоимостью" + productCost + "руб. успешно добавлен.");
+            System.out.printf("Товар '%s' стоимостью %.2f рубл%s успешно добавлен.\n", productName,
+                    productCost, properTextEnd(productCost));
 
-            System.out.println("Хотите добавить еще товар?\nЕсли хотите завершить добавление товаров, введите команду 'Завершить'");
+            System.out.println("Хотите добавить еще товар? (Для завершения введите команду 'Завершить')");
             command = scanner.next().toLowerCase();
-            if (command.equals("завершить")) {
-                productHashMap.put(productList, totalCost);
-                break;
-            }
         }
+        productHashMap.put(productList, totalCost);
         return productHashMap;
     }
 }
